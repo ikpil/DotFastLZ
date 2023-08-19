66638c59f306a56af5a73b78a1f014d6812521d4
ikpil <ikpil0@gmail.com>
2023-08-19 16:40:27 +0900

    ready test

--- test/DotFastLZ.Compression.Tests/RoundtripTests.cs
+++ test/DotFastLZ.Compression.Tests/RoundtripTests.cs
@@ -65,7 +65,7 @@ public class RoundtripTests
         Console.WriteLine($"Processing {name}...");
         if (!File.Exists(file_name))
         {
-            Console.WriteLine("Error: can not open %s!", file_name);
+            Console.WriteLine($"Error: can not open {file_name}!");
             return false;
         }
 
@@ -127,7 +127,7 @@ public class RoundtripTests
         Console.WriteLine($"Processing {name}...");
         if (!File.Exists(file_name))
         {
-            Console.WriteLine("Error: can not open %s!", file_name);
+            Console.WriteLine($"Error: can not open {file_name}!");
             return false;
         }
 
@@ -195,7 +195,7 @@ public class RoundtripTests
         Console.WriteLine($"Processing {name}...");
         if (!File.Exists(file_name))
         {
-            Console.WriteLine("Error: can not open %s!", file_name);
+            Console.WriteLine($"Error: can not open {file_name}!");
             return false;
         }
 
@@ -246,7 +246,7 @@ public class RoundtripTests
 
         Console.WriteLine($"{name} {file_size} -> {compressed_size} ({ratio})");
 
-        return false;
+        return true;
     }
 
     /*
@@ -255,69 +255,63 @@ public class RoundtripTests
       Decompress the output with Level 2 decompressor.
       Compare the result with the original file content.
     */
-    void test_roundtrip_level2(string name, string file_name)
+    bool test_roundtrip_level2(string name, string file_name)
     {
-        // Console.WriteLine("Processing %s...", name);
-        // FILE* f = fopen(file_name, "rb");
-        // if (!f)
-        // {
-        //     Console.WriteLine("Error: can not open %s!", file_name);
-        //     exit(1);
-        // }
-        //
-        // fseek(f, 0L, SEEK_END);
-        // long file_size = ftell(f);
-        // rewind(f);
-        //
-        // Console.WriteLine("Size is %ld bytes.", file_size);
-        // if (file_size > MAX_FILE_SIZE)
-        // {
-        //     fclose(f);
-        //     Console.WriteLine("%25s %10ld [skipped, file too big]", name, file_size);
-        //     return;
-        // }
-        //
-        // uint8_t* file_buffer = malloc(file_size);
-        // long read = fread(file_buffer, 1, file_size, f);
-        // fclose(f);
-        // if (read != file_size)
-        // {
-        //     free(file_buffer);
-        //     Console.WriteLine("Error: only read %ld bytes!", read);
-        //     exit(1);
-        // }
-        //
-        // Console.WriteLine("Compressing. Please wait...");
-        // uint8_t* compressed_buffer = malloc(1.05 * file_size);
-        // int compressed_size = fastlz_compress_level(2, file_buffer, file_size, compressed_buffer);
-        // double ratio = (100.0 * compressed_size) / file_size;
-        // Console.WriteLine("Compressing was completed: %ld -> %ld (%.2f%%)", file_size, compressed_size, ratio);
-        //
-        // Console.WriteLine("Decompressing. Please wait...");
-        // uint8_t* uncompressed_buffer = malloc(file_size);
-        // if (uncompressed_buffer == NULL)
-        // {
-        //     free(file_buffer);
-        //     free(compressed_buffer);
-        //     Console.WriteLine("%25s %10ld  -> %10d  (%.2f%%)  skipped, can't decompress OOM", name, file_size, compressed_size, ratio);
-        //     exit(1);
-        //     return;
-        // }
-        //
-        // memset(uncompressed_buffer, '-', file_size);
+        Console.WriteLine($"Processing {name}...");
+        if (!File.Exists(file_name))
+        {
+            Console.WriteLine($"Error: can not open {file_name}!");
+            return false;
+        }
+
+        var fileInfo = new FileInfo(file_name);
+        var file_size = fileInfo.Length;
+        Console.WriteLine($"Size is {file_size} bytes.");
+
+        if (file_size > MAX_FILE_SIZE)
+        {
+            Console.WriteLine($"{name} {file_size} [skipped, file too big]");
+            return false;
+        }
+
+        using var fs = new FileStream(file_name, FileMode.Open, FileAccess.Read, FileShare.Read);
+        byte[] file_buffer = new byte[file_size];
+        int read = fs.Read(file_buffer, 0, file_buffer.Length);
+        if (read != file_size)
+        {
+            Console.WriteLine($"Error: only read {read} bytes!");
+            return false;
+        }
+
+        Console.WriteLine("Compressing. Please wait...");
+
+        byte[] compressed_buffer = new byte[(int)(1.05 * file_size)];
+        int compressed_size = FastLZ.Compress(file_buffer, 0, (int)file_size, compressed_buffer, 0, 2);
+        double ratio = (100.0 * compressed_size) / file_size;
+        Console.WriteLine($"Compressing was completed: {file_size} -> {compressed_size} ({ratio})");
+
+        Console.WriteLine("Decompressing. Please wait...");
+        byte[] uncompressed_buffer = new byte[file_size];
+        if (null == uncompressed_buffer)
+        {
+            Console.WriteLine($"{name} {file_size} -> {compressed_size}  ({ratio})  skipped, can't decompress");
+            return false;
+        }
+
+        Array.Fill(uncompressed_buffer, (byte)'-');
+
         // fastlz_decompress(compressed_buffer, compressed_size, uncompressed_buffer, file_size);
-        // Console.WriteLine("Comparing. Please wait...");
-        // int result = compare(file_name, file_buffer, uncompressed_buffer, file_size);
-        // if (result == 1)
-        // {
-        //     free(uncompressed_buffer);
-        //     exit(1);
-        // }
-        //
-        // free(file_buffer);
-        // free(compressed_buffer);
-        // free(uncompressed_buffer);
-        // Console.WriteLine("%25s %10ld  -> %10d  (%.2f%%)", name, file_size, compressed_size, ratio);
+
+        Console.WriteLine("Comparing. Please wait...");
+        long result = compare(file_name, file_buffer, uncompressed_buffer, file_size);
+        if (0 <= result)
+        {
+            return false;
+        }
+
+        Console.WriteLine($"{name} {file_size} -> {compressed_size} ({ratio})");
+
+        return true;
     }
 
 
@@ -380,7 +374,7 @@ public class RoundtripTests
         foreach (var name in _names)
         {
             var filename = prefix + name;
-            //   test_roundtrip_level1(name, filename);
+            test_roundtrip_level1(name, filename);
         }
 
         Console.WriteLine("");
@@ -389,7 +383,7 @@ public class RoundtripTests
         foreach (var name in _names)
         {
             var filename = prefix + name;
-            //   test_roundtrip_level2(name, filename);
+            test_roundtrip_level2(name, filename);
         }
 
         Console.WriteLine("");
