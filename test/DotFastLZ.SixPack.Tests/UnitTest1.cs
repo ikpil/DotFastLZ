9e88c7bf85b56363fb7584a8ed40554e48ac688b
ikpil <ikpil@naver.com>
2023-09-25 23:07:17 +0900

    feature: add sixpack

---
+++ test/DotFastLZ.SixPack.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace DotFastLZ.SixPack.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
