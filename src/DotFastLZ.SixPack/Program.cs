c9cf188b30aeb068c99f18be0170b72944df2382
ikpil <ikpil@naver.com>
2023-09-25 23:07:17 +0900

    feature: add sixpack

---
+++ src/DotFastLZ.SixPack/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotFastLZ.SixPack;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+        return 0;
+    }
+}
