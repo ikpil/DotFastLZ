aa39585f4cf9d3839178939ed255044e700223f6
ikpil <ikpil0@gmail.com>
2023-10-03 11:19:34 +0900

    feat: simple

--- README.md
+++ README.md
@@ -9,38 +9,17 @@ DotFastLZ is a C# port of [ariya/FastLZ](https://github.com/ariya/FastLZ) for C#
 
 ## Usage: FastLZ
 ```csharp
-// input source
-string text = "This is an example original text in English. ";
-
-// extend the text to meet a minimum length of 2048 characters
-while (text.Length < 2048)
-{
-    text += "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n";
-}
-
-var input = Encoding.UTF8.GetBytes(text);
-
 for (int level = 1; level <= 2; ++level)
 {
     // compress
+    var input = GetInputSource();
     var estimateSize = FastLZ.EstimateCompressedSize(input.Length);
-    var comBuf = new byte[estimateSize];
+    var comBuf = new byte[estimateSize]~~~~;
     var comBufSize = FastLZ.CompressLevel(level, input, input.Length, comBuf);
 
     // decompress
-    byte[] decBuf = null;
-    var decBufSize = 0L;
-    do
-    {
-        // guess
-        long guessSize = null == decBuf 
-            ? comBufSize * 3 
-            : decBuf.Length * 3;
-
-        decBuf = new byte[guessSize];
-        decBufSize = FastLZ.Decompress(comBuf, comBufSize, decBuf, decBuf.Length);
-        // ..
-    } while (0 == decBufSize && decBuf.Length < input.Length);
+    byte[] decBuf = new byte[input.Length];
+    var decBufSize = FastLZ.Decompress(comBuf, comBufSize, decBuf, decBuf.Length);
 
     // compare
     var compareSize = FastLZ.MemCompare(input, 0, decBuf, 0, decBufSize);
--- test/DotFastLZ.Compression.Tests/FastLZTests.cs
+++ test/DotFastLZ.Compression.Tests/FastLZTests.cs
@@ -6,8 +6,7 @@ namespace DotFastLZ.Compression.Tests;
 
 public class FastLZTests
 {
-    [Test]
-    public void TestUsage()
+    public static byte[] GetInputSource()
     {
         // input source
         string text = "This is an example original text in English. ";
@@ -18,29 +17,23 @@ public class FastLZTests
             text += "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n";
         }
 
-        var input = Encoding.UTF8.GetBytes(text);
+        return Encoding.UTF8.GetBytes(text);
+    }
 
+    [Test]
+    public void TestUsage()
+    {
         for (int level = 1; level <= 2; ++level)
         {
             // compress
+            var input = GetInputSource();
             var estimateSize = FastLZ.EstimateCompressedSize(input.Length);
             var comBuf = new byte[estimateSize];
             var comBufSize = FastLZ.CompressLevel(level, input, input.Length, comBuf);
 
             // decompress
-            byte[] decBuf = null;
-            var decBufSize = 0L;
-            do
-            {
-                // guess
-                long guessSize = null == decBuf 
-                    ? comBufSize * 3 
-                    : decBuf.Length * 3;
-        
-                decBuf = new byte[guessSize];
-                decBufSize = FastLZ.Decompress(comBuf, comBufSize, decBuf, decBuf.Length);
-                // ..
-            } while (0 == decBufSize && decBuf.Length < input.Length);
+            byte[] decBuf = new byte[input.Length];
+            var decBufSize = FastLZ.Decompress(comBuf, comBufSize, decBuf, decBuf.Length);
 
             // compare
             var compareSize = FastLZ.MemCompare(input, 0, decBuf, 0, decBufSize);
